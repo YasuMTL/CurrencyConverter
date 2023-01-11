e21ef13ca4efdeeb4807fab5e5afb9a6b82a6feb
Yasunari <45267210+YasuMTL@users.noreply.github.com>
2023-01-11 14:16:46 -0500

    Reformat code in MainActivity.kt
    Signed-off-by: Yasunari <45267210+YasuMTL@users.noreply.github.com>

--- app/src/main/java/com/yasu_k/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/yasu_k/currencyconverter/MainActivity.kt
@@ -16,8 +16,7 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
-    companion object
-    {
+    companion object {
         private lateinit var mViewModel: ConverterViewModel
         private var result: RateXmlResponse? = null
     }
@@ -28,7 +27,8 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
         mViewModel = ViewModelProvider(this)[ConverterViewModel::class.java]
 
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.viewModel = mViewModel
         binding.lifecycleOwner = this
 
@@ -41,11 +41,10 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
         setAdView()
     }
-    
-    private fun setAdView()
-    {
+
+    private fun setAdView() {
         val mAdView: AdView = findViewById(R.id.adView)
-        MobileAds.initialize(this){}
+        MobileAds.initialize(this) {}
         val adRequest = AdRequest.Builder().build()
         mAdView.loadAd(adRequest)
         mAdView.adListener = object : AdListener() {
@@ -59,8 +58,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         }
     }
 
-    private fun getCurrencyList(): List<String>
-    {
+    private fun getCurrencyList(): List<String> {
         val currencyList = mutableListOf<String>()
         val currencies = result?.currencyRates
 
@@ -76,46 +74,46 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         return currencyList
     }
 
-    private fun setSpinners()
-    {
+    private fun setSpinners() {
         val spinnerCurrencyFrom: Spinner = findViewById(R.id.spinnerCurrencyFrom)
         val spinnerCurrencyTo: Spinner = findViewById(R.id.spinnerCurrencyTo)
 
         val currencyList = getCurrencyList()
         val flagList: Array<Int> = arrayOf(
-                    R.drawable.au,
-                    R.drawable.bg,
-                    R.drawable.br,
-                    R.drawable.ca,
-                    R.drawable.ch,
-                    R.drawable.cn,
-                    R.drawable.cz,
-                    R.drawable.dk,
-                    R.drawable.eu,
-                    R.drawable.gb,
-                    R.drawable.hk,
-                    //R.drawable.hr,
-                    R.drawable.hu,
-                    R.drawable.id,
-                    R.drawable.il,
-                    R.drawable.ind,
-                    R.drawable.isl,
-                    R.drawable.jp,
-                    R.drawable.kr,
-                    R.drawable.mx,
-                    R.drawable.my,
-                    R.drawable.no,
-                    R.drawable.nz,
-                    R.drawable.ph,
-                    R.drawable.pl,
-                    R.drawable.ro,
-                    //R.drawable.ru,
-                    R.drawable.se,
-                    R.drawable.sg,
-                    R.drawable.th,
-                    R.drawable.tr,
-                    R.drawable.us,
-                    R.drawable.za)
+            R.drawable.au,
+            R.drawable.bg,
+            R.drawable.br,
+            R.drawable.ca,
+            R.drawable.ch,
+            R.drawable.cn,
+            R.drawable.cz,
+            R.drawable.dk,
+            R.drawable.eu,
+            R.drawable.gb,
+            R.drawable.hk,
+            //R.drawable.hr,
+            R.drawable.hu,
+            R.drawable.id,
+            R.drawable.il,
+            R.drawable.ind,
+            R.drawable.isl,
+            R.drawable.jp,
+            R.drawable.kr,
+            R.drawable.mx,
+            R.drawable.my,
+            R.drawable.no,
+            R.drawable.nz,
+            R.drawable.ph,
+            R.drawable.pl,
+            R.drawable.ro,
+            //R.drawable.ru,
+            R.drawable.se,
+            R.drawable.sg,
+            R.drawable.th,
+            R.drawable.tr,
+            R.drawable.us,
+            R.drawable.za
+        )
 
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, currencyList)
         val flagAdapter = FlagAdapter(applicationContext, flagList, currencyList)
@@ -129,32 +127,26 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         spinnerCurrencyTo.onItemSelectedListener = this
     }
 
-    fun calculateRate
-    (
-        currencyFrom:String,
-        currencyTo:String,
-        apiResponse:RateXmlResponse
-    ):Double
-    {
-        val result:Double
+    fun calculateRate(
+        currencyFrom: String,
+        currencyTo: String,
+        apiResponse: RateXmlResponse
+    ): Double {
+        val result: Double
 
         when {
             currencyFrom == currencyTo -> result = 1.0
             currencyTo == "EUR" -> result = 1 / apiResponse.getRate(currencyFrom)
             currencyFrom == "EUR" -> result = apiResponse.getRate(currencyTo)
 
-            else ->
-            {
+            else -> {
                 val from = apiResponse.getRate(currencyFrom)
                 val to = apiResponse.getRate(currencyTo)
 
                 result =
-                    if(from != 0.0)
-                    {
+                    if (from != 0.0) {
                         to / from
-                    }
-                    else
-                    {
+                    } else {
                         0.0
                     }
             }
@@ -163,10 +155,8 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         return result
     }
 
-    private fun getCurrentRate()
-    {
-        if (result != null)
-        {
+    private fun getCurrentRate() {
+        if (result != null) {
             val currencyFrom = mViewModel.currencyFrom.value
             val currencyTo = mViewModel.currencyTo.value
 
@@ -175,17 +165,14 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             mViewModel.exchangeRates.postValue(result)//--> How to check the value in livedata??
 
             Log.d("tvRate", "I'm gonna update the exchange rate!!")
-        }
-        else
-        {
+        } else {
             mViewModel.exchangeRates.postValue(null)
-            Log.e("getCurrentRate","API call fail")
+            Log.e("getCurrentRate", "API call fail")
         }
     }
 
-    override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long)
-    {
-        when (parent.id){
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+        when (parent.id) {
             R.id.spinnerCurrencyFrom -> {
                 mViewModel.currencyFrom.value = parent.getItemAtPosition(position).toString()
                 getCurrentRate()
